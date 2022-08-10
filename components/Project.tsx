abf00a5811549448b3627bbe2aedcc85cb43ef22
Oscar Medina <omedinapr@gmail.com>
2022-08-10 08:01:05 -0400

    Made Project component responsive

--- components/Project.tsx
+++ components/Project.tsx
@@ -23,14 +23,14 @@ const Project = (props: IProjectProps) => {
                 opacity: isInView ? 1 : 0,
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
             }}
-            className="flex items-start mb-32"
+            className="flex flex-wrap lg:flex-nowrap items-start mb-32 p-8 lg:p-0"
         >
-            <div className="z-20 w-1/3 text-right">
+            <div className="z-20 w-full lg:w-1/3 text-center lg:text-right">
                 {/* Project Title */}
                 <div className="text-4xl font-semibold uppercase">{props.title}</div>
 
                 {/* Project Description */}
-                <div className="p-4 my-4 bg-gray-50 rounded-xl bg-opacity-90 w-[130%] text-medina-red font-semibold text-lg text-left leading-6">
+                <div className="p-4 my-4 bg-gray-50 rounded-xl bg-opacity-90 w-full lg:w-[130%] text-medina-red font-semibold text-lg leading-6 text-center lg:text-left">
                     {props.description}
                 </div>
 
@@ -48,18 +48,9 @@ const Project = (props: IProjectProps) => {
                     }
                 </div>
             </div>
-            <div className='z-10 w-2/3 ml-8'>
+            <div className='z-10 w-full lg:w-2/3 lg:ml-8 pt-4 lg:pt-0'>
                 {/* Project Image */}
                 <Image layout={"responsive"} src={props.image} width={1420} height={458} alt={`Project ${props.title}`} />
-
-                {/* Project Screenshots */}
-                {/* <div className='flex items-center justify-end mt-4'>
-                    <div className='w-16 h-16 mr-4 border rounded-lg'></div>
-                    <div className='w-16 h-16 mr-4 border rounded-lg'></div>
-                    <div className='w-16 h-16 mr-4 border rounded-lg'></div>
-                    <div className='w-16 h-16 mr-4 border rounded-lg'></div>
-                    <div className='w-16 h-16 mr-4 border rounded-lg'></div>
-                </div> */}
             </div>
         </div>
     );
